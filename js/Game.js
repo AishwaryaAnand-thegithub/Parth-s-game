f8bc3a62f36049e4be103ae4dfbc0981514519c3
AishwaryaAnand-thegithub <69575537+AishwaryaAnand-thegithub@users.noreply.github.com>
2021-06-18 20:30:17 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,86 @@
+class Game {
+  constructor(){
+
+  }
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
+      form = new Form()
+      form.display();
+    }
+
+    car1 = createSprite(30,250,30,80);
+    car2 = createSprite(460,250,30,80);
+   
+    cars = [car1, car2];
+  }
+
+  play(){
+    form.hide();
+
+    Player.getPlayerInfo();
+    
+    if(allPlayers !== undefined){
+      //var display_position = 100;
+      
+      //index of the array
+      var index = 0;
+
+      //x and y position of the cars
+      var x = 30;
+      var y=250;
+
+      for(var plr in allPlayers){
+        //add 1 to the index for every loop
+        index = index + 1 ;
+
+        //position the cars a little away from each other in x direction
+        x = x + 430;
+        //use data form the database to display the cars in y direction
+        y = 500 - allPlayers[plr].distance;
+        cars[index-1].x = x;
+        cars[index-1].y = y;
+
+        if (index === player.index){
+          cars[index - 1].shapeColor = "red";
+        
+        }
+       
+        //textSize(15);
+        //text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+      }
+
+    }
+
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+      player.distance +=10
+      player.update();
+    }
+    if(keyIsDown(DOWN_ARROW) && player.index !== null){
+      player.distance -=10
+      player.update();
+    }
+
+    drawSprites();
+  }
+}
